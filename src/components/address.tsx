f0595e6b9c580b6540378f950657d471abe9b67b
mykeduong <hongminh4402@gmail.com>
2025-02-17 16:59:13 -0800

    Finished

--- src/components/address.tsx
+++ src/components/address.tsx
@@ -9,16 +9,22 @@ interface Props {
   app: any
 }
 
+enum Status { Initial, Sent, Sending, Failed }
+
 const Address = ({ goBack, app }: Props) => {
   const db = getFirestore(app);
   const [address, setAddress] = useState<string>("")
+  const [status, setStatus] = useState(Status.Initial)
 
   const sendToFirestore = async () => {
     try {
+      setStatus(Status.Sending)
       console.log("Here")
       await addDoc(collection(db, "address"), { address });
+      setStatus(Status.Sent)
     } catch (e) {
       console.error("Error adding document: ", e);
+      setStatus(Status.Failed)
     }
   }
 
@@ -49,9 +55,14 @@ const Address = ({ goBack, app }: Props) => {
         onChange={(e: React.ChangeEvent<HTMLInputElement>) => setAddress(e.target.value)}
       />
       <div className={`h-10`} />
-      <button className={`px-10 py-4 bg-gray-800 text-white text-4xl rounded-lg`} onClick={() => sendToFirestore()}>
-        Gửi
-      </button>
+      {status === Status.Initial ?
+        <button className={`px-10 py-4 bg-gray-800 text-white text-4xl rounded-lg`} onClick={() => sendToFirestore()}>
+          Gửi
+        </button>
+        : status === Status.Sending ?
+          <p className={`text-3xl`}>...</p> :
+          status === Status.Sent ? <p className={`text-3xl`}>Anh xin nha :D</p> : <p className={`text-3xl`}>Thằng làm app ngoo vl...Có gì thử lại giúp anh nha</p>
+      }
     </div>
   )
 
